e45e74b4048500dea5a451c6e33574eaef2e8b5d
biko-the-bird <34891228+biko-the-bird@users.noreply.github.com>
2018-10-02 08:34:02 -0400

    added files

---
+++ routes/gun.js
@@ -0,0 +1,19 @@
+var Gun = require('gun')
+module.exports = function on(app){
+
+    app.put(`/gunDB`, function(req, res) {
+        console.log("hit gun");
+        if (Gun.serve(req, res)){ return } // filters gun requests!
+        require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error',function(){  
+        
+        res.send({"success": true});
+        res.end();
+        }).pipe(res); // stream
+      
+      
+    })
+
+  
+
+   
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser')
+
+var fs = require('fs');
+var path = require('path');
+
+
+app.use(bodyParser.json({limit: "50mb"}))
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+function recursiveRoutes(folderName) {
+  fs.readdirSync(folderName).forEach(function(file) {
+
+      var fullName = path.join(folderName, file);
+      var stat = fs.lstatSync(fullName);
+
+      if (stat.isDirectory()) {
+          recursiveRoutes(fullName);
+      } else if (file.toLowerCase().indexOf('.js')) {
+          require('./' + fullName)(app);
+          console.log("require('" + fullName + "')");
+      }
+  });
+}
+recursiveRoutes('routes'); // Initialize it
+
+
+app.get('/health-check', (req, res) => res.sendStatus(200));
+
+
+
+const port = 5000;
+
+app.listen(port, () => {console.log(`Server running on port ${port}`, "mello")});
+console.log("here");
